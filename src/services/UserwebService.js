fe10da863d0f24ae0bdb2f78be1779805e47a084
ArtemisLuna123 <mplazcano08@gmail.com>
2023-04-04 16:54:40 -0400

    Se cambiaron los services de userweb

--- src/services/UserwebService.js
+++ src/services/UserwebService.js
@@ -8,5 +8,26 @@ const getUserwebFirstLastName = async() => {
     return rest.data;
 }
 
+const userwebAdd = async (userweb)  => {
+    const rest =  await axios.post(baseURL+"/userweb/Save", userweb);
+     console.log(rest);
+     return rest.data;
+}
 
-export { getUserwebFirstLastName }
+const userwebEdit = async (userweb) => {
+    const rest = await  axios.put(baseURL+"/userweb/Update",userweb);
+    console.log(rest);
+    return rest.data;
+}
+
+
+const userwebDelete = async (id) => {
+    const rest = await  axios.delete(baseURL+"/userweb/Delete/"+ id);
+    console.log(rest);
+    
+}
+
+
+
+
+export { getUserwebFirstLastName,userwebAdd, userwebEdit,userwebDelete}
