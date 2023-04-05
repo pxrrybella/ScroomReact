7e45d90676edec0f5da796c053d1d9e350a56ae8
ArtemisLuna123 <mplazcano08@gmail.com>
2023-04-05 17:01:14 -0400

    Se agregaron preguntas nuevas

--- src/components/Survey.js
+++ src/components/Survey.js
@@ -6,67 +6,126 @@ const Survey = () => {
             <div id="carouselExample" class="carousel slide">
                 <div class="carousel-inner">
                     <figure className="text-center">
-                        <div class="carousel-item carousel-item-survey active">
+                    <div class="carousel-item carousel-item-survey active">
                             <blockquote class="blockquote">
-                                <p class="mt-4">¿Qué tan estresado te sentiste en este Sprint?</p>
-                                <div class="d-grid gap-1 col-6 mx-auto survey-options">
-                                    <button type="button" class="btn btn-dark">Nada motivado</button>
-                                    <button type="button" class="btn btn-dark">Poco motivado</button>
-                                    <button type="button" class="btn btn-dark">Ni motivado, ni desmotivado</button>
-                                    <button type="button" class="btn btn-dark">Motivado</button>
-                                    <button type="button" class="btn btn-dark">Muy motivado</button>
-                                </div>
-                            </blockquote>
-                        </div>
-                        <div class="carousel-item carousel-item-survey">
-                            <blockquote class="blockquote">
-                                <p class="mt-4">¿Qué tan motivado te has sentido en este Sprint?</p>
-                                <div class="d-grid gap-1 col-6 mx-auto survey-options">
-                                    <button type="button" class="btn btn-dark">Nada estresado</button>
-                                    <button type="button" class="btn btn-dark">Poco Estresado</button>
-                                    <button type="button" class="btn btn-dark">Medianamente estresado</button>
-                                    <button type="button" class="btn btn-dark">Estresado</button>
-                                    <button type="button" class="btn btn-dark">Muy Estresado</button>
-                                </div>
-                            </blockquote>
-                        </div>
-                        <div class="carousel-item carousel-item-survey">
-                            <blockquote class="blockquote">
-                                <p class="mt-4">Los líderes animan a que los demas entreguen opiniones distintas a las suyas</p>
-                                <div class="d-grid gap-1 col-6 mx-auto survey-options">
-                                    <button type="button" class="btn btn-dark">Muy en desacuerdo</button>
-                                    <button type="button" class="btn btn-dark">En desacuerdo</button>
-                                    <button type="button" class="btn btn-dark">Neutro</button>
-                                    <button type="button" class="btn btn-dark">De acuerdo</button>
+                                <p class="mt">¿Tienes todas las herramientas necesarias para hacer tu mejor trabajo?</p>
+                                <div class="d-grid gap-2 col-6 mx-auto">
+                                    <button type="button" class="btn btn-dark">En nada de acuerdo</button>
+                                    <button type="button" class="btn btn-dark">Poco de acuerdo</button>
+                                    <button type="button" class="btn btn-dark">Neutral</button>
+                                    <button type="button" class="btn btn-dark">De acuerdo </button>
                                     <button type="button" class="btn btn-dark">Muy de acuerdo</button>
                                 </div>
                             </blockquote>
                         </div>
                         <div class="carousel-item carousel-item-survey">
                             <blockquote class="blockquote">
-                                <p class="mt-4">Todos reciben un trato respetuoso y digno</p>
-                                <div class="d-grid gap-1 col-6 mx-auto survey-options">
-                                    <button type="button" class="btn btn-dark">Muy en desacuerdo</button>
-                                    <button type="button" class="btn btn-dark">En desacuerdo</button>
-                                    <button type="button" class="btn btn-dark">Neutro</button>
-                                    <button type="button" class="btn btn-dark">De acuerdo</button>
+                                <p class="mt-4 mb-3">¿Está satisfecho con las oportunidades de crecimiento de tu carrera en esta empresa, y hay un plan claro disponible para hacerlo?</p>
+                                <div class="d-grid gap-2 col-5 mx-auto">
+                                    <button type="button" class="btn btn-dark">En nada de acuerdo</button>
+                                    <button type="button" class="btn btn-dark">Poco de acuerdo</button>
+                                    <button type="button" class="btn btn-dark">Neutral</button>
+                                    <button type="button" class="btn btn-dark">De acuerdo </button>
                                     <button type="button" class="btn btn-dark">Muy de acuerdo</button>
                                 </div>
                             </blockquote>
                         </div>
                         <div class="carousel-item carousel-item-survey">
                             <blockquote class="blockquote">
-                            <p className="mt-4">Tengo suficiente tiempo para terminar con mi trabajo en la jornada. asdasdadasdad adsda sdasd dsadsad asdasdasd asdasda .</p>
-                                <div class="d-grid gap-1 col-6 mx-auto survey-options">
-                                    <button type="button" class="btn btn-dark">Muy en desacuerdo</button>
-                                    <button type="button" class="btn btn-dark">En desacuerdo</button>
-                                    <button type="button" class="btn btn-dark">Neutro</button>
-                                    <button type="button" class="btn btn-dark">De acuerdo</button>
+                                <p class="mt-5 mb-3">¿Tu gerente te comunica las expectativas y te da retroalimentación de manera clara y profesional?</p>
+                                <div class="d-grid gap-2 col-6 mx-auto">
+                                    <button type="button" class="btn btn-dark">En nada de acuerdo</button>
+                                    <button type="button" class="btn btn-dark">Poco de acuerdo</button>
+                                    <button type="button" class="btn btn-dark">Neutral</button>
+                                    <button type="button" class="btn btn-dark">De acuerdo </button>
+                                    <button type="button" class="btn btn-dark">Muy de acuerdo</button>
+                                </div>
+                            </blockquote>
+                        </div>
+                        <div class="carousel-item carousel-item-survey">
+                            <blockquote class="blockquote">
+                                <p class="mt-5">¿Tu equipo/departamento apoya tu trabajo y te inspira a mejorar?</p>
+                                <div class="d-grid gap-2 col-6 mx-auto">
+                                    <button type="button" class="btn btn-dark">En nada de acuerdo</button>
+                                    <button type="button" class="btn btn-dark">Poco de acuerdo</button>
+                                    <button type="button" class="btn btn-dark">Neutral</button>
+                                    <button type="button" class="btn btn-dark">De acuerdo </button>
+                                    <button type="button" class="btn btn-dark">Muy de acuerdo</button>
+                                </div>
+                            </blockquote>
+                        </div>
+                        <div class="carousel-item carousel-item-survey">
+                            <blockquote class="blockquote">
+                                <p class="mt-5">¿Te sientes valorado como empleado de esta organización?</p>
+                                <div class="d-grid gap-2 col-6 mx-auto">
+                                    <button type="button" class="btn btn-dark">En nada de acuerdo</button>
+                                    <button type="button" class="btn btn-dark">Poco de acuerdo</button>
+                                    <button type="button" class="btn btn-dark">Neutral</button>
+                                    <button type="button" class="btn btn-dark">De acuerdo </button>
+                                    <button type="button" class="btn btn-dark">Muy de acuerdo</button>
+                                </div>
+                            </blockquote>
+                        </div>
+                        <div class="carousel-item carousel-item-survey">
+                            <blockquote class="blockquote">
+                                <p class="mt-5">¿Está clara la declaración de la misión de la empresa y te inspira a contribuir con tu mejor trabajo?</p>
+                                <div class="d-grid gap-2 col-6 mx-auto">
+                                    <button type="button" class="btn btn-dark">En nada de acuerdo</button>
+                                    <button type="button" class="btn btn-dark">Poco de acuerdo</button>
+                                    <button type="button" class="btn btn-dark">Neutral</button>
+                                    <button type="button" class="btn btn-dark">De acuerdo </button>
+                                    <button type="button" class="btn btn-dark">Muy de acuerdo</button>
+                                </div>
+                            </blockquote>
+                        </div>
+                        <div class="carousel-item carousel-item-survey">
+                            <blockquote class="blockquote">
+                                <p class="mt-5">¿Crees que la empresa fomenta la inclusión y protege a los empleados de la discriminación y el acoso?</p>
+                                <div class="d-grid gap-2 col-6 mx-auto">
+                                    <button type="button" class="btn btn-dark">En nada de acuerdo</button>
+                                    <button type="button" class="btn btn-dark">Poco de acuerdo</button>
+                                    <button type="button" class="btn btn-dark">Neutral</button>
+                                    <button type="button" class="btn btn-dark">De acuerdo </button>
+                                    <button type="button" class="btn btn-dark">Muy de acuerdo</button>
+                                </div>
+                            </blockquote>
+                        </div>
+                        <div class="carousel-item carousel-item-survey">
+                            <blockquote class="blockquote">
+                                <p class="mt-5">¿Te aseguras de asumir la responsabilidad de tus acciones y decisiones en el trabajo?</p>
+                                <div class="d-grid gap-2 col-6 mx-auto">
+                                    <button type="button" class="btn btn-dark">En nada de acuerdo</button>
+                                    <button type="button" class="btn btn-dark">Poco de acuerdo</button>
+                                    <button type="button" class="btn btn-dark">Neutral</button>
+                                    <button type="button" class="btn btn-dark">De acuerdo </button>
+                                    <button type="button" class="btn btn-dark">Muy de acuerdo</button>
+                                </div>
+                            </blockquote>
+                        </div>
+                        <div class="carousel-item carousel-item-survey">
+                            <blockquote class="blockquote">
+                                <p class="mt-5">¿Te aseguras de aprender de los éxitos y logros en tu trabajo y de seguir mejorando en el futuro?</p>
+                                <div class="d-grid gap-2 col-6 mx-auto">
+                                    <button type="button" class="btn btn-dark">En nada de acuerdo</button>
+                                    <button type="button" class="btn btn-dark">Poco de acuerdo</button>
+                                    <button type="button" class="btn btn-dark">Neutral</button>
+                                    <button type="button" class="btn btn-dark">De acuerdo </button>
+                                    <button type="button" class="btn btn-dark">Muy de acuerdo</button>
+                                </div>
+                            </blockquote>
+                        </div>
+                        <div class="carousel-item carousel-item-survey">
+                            <blockquote class="blockquote">
+                                <p class="mt-3 mb-3">¿Te aseguras de ser transparente y honesto en tu comunicación y colaboración con tus colegas y superiores?</p>
+                                <div class="d-grid gap-2 col-6 mx-auto sm-2">
+                                    <button type="button" class="btn btn-dark">En nada de acuerdo</button>
+                                    <button type="button" class="btn btn-dark">Poco de acuerdo</button>
+                                    <button type="button" class="btn btn-dark">Neutral</button>
+                                    <button type="button" class="btn btn-dark">De acuerdo </button>
                                     <button type="button" class="btn btn-dark">Muy de acuerdo</button>
                                 </div>
                             </blockquote>
                             <button type="button" class="btn btn-primary mt-1 survey-send" >Enviar</button>
-                            
                         </div>
                         
                     </figure>
