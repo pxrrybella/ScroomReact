5075950c313eb0bfe799eb815cec3c5887d854c6
ArtemisLuna123 <mplazcano08@gmail.com>
2023-04-10 10:12:06 -0400

    Se agrego el company delete y save

--- src/services/CompanyService.js
+++ src/services/CompanyService.js
@@ -6,4 +6,18 @@ const companyGetAll = async(company) => {
     const rest = await axios.get(baseURL + '/findAll');
     return rest.data;
 }
-export {companyGetAll}
+
+const companyAdd = async (company)  => {
+    const rest =  await axios.post(baseURL+"/save", company);
+     console.log(rest);
+     return rest.data;
+}
+
+const companyDelete = async (id) => {
+    const rest = await  axios.delete(baseURL+'/delete/'+ id);
+    console.log(rest);
+    return rest.data;
+    
+}
+
+export {companyGetAll,companyAdd,companyDelete}
