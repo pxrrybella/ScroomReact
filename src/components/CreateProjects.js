4d165d610ca3d6a8a0305d46bc83175a4bd2d21a
pxrrybella <isabellapu45@gmail.com>
2023-04-10 10:32:14 -0400

    Project code added

--- src/components/CreateProjects.js
+++ src/components/CreateProjects.js
@@ -20,21 +20,26 @@ const projectDefault = {
 
 const CreateProject = ( {agregarProject} ) => {
 
+    function codeGenerate(){
+        const code = (Math.floor(1000000 + Math.random() * 9000000));
+        return code;
+    }
+
     const [project, setProject] = useState(projectDefault);
-    const { project_id, project_name, admin_name } = project;
+    const { project_id, project_name, admin_name, project_code } = project;
     const handleInputChange = (p) => {
         const handleInputChange = {
             ...project,
             [p.target.name]: p.target.value,
+            "project_code" : codeGenerate()
         }
         setProject(handleInputChange);
     }
 
-
     function handleSubmit(e) {
         e.preventDefault();
         console.log('You clicked submit.');
-        alert(project.project_id);
+        alert('El código de tu proyecto "' + project.project_name + '" tiene código: ' + project.project_code);
       }
 
     return (
@@ -42,8 +47,11 @@ const CreateProject = ( {agregarProject} ) => {
             <h2 className="mb-4">Nombre del proyecto</h2>
             <form onSubmit={handleSubmit}>
             <input type="text" max={30} className="form-control form-control-lg text-center" placeholder="Ingrese aquí el nombre del proyecto" id="project_name" name="project_name" value={project_name} onChange={handleInputChange}></input>
-            <button type="submit" className="btn btn-primary mt-4" onClick={() => agregarProject(project)}>Siguiente</button>
-            {project.project_name!==undefined ? (<div className="text-center"> <h1>Tu código: {project.project_id}</h1> <Link to="/DashboardPage">Siguiente</Link></div>):(<></>)}
+            {/* project.project_name!=="" ? (<div className="text-center"> <h1>Tu código: {codeGenerate()}</h1> <Link to="/DashboardPage">Siguiente</Link></div>):(<></>)*/}
+            <div className="d-flex align-items-center justify-content-center">
+            <button type="submit" className="btn btn-primary mt-4" onClick={() => agregarProject(project)}>Obtener código</button>
+            <Link to="/DashboardPage" className="ms-2"><button type="submit" className="btn btn-primary mt-4">Ir al Dashboard</button></Link>
+            </div>
             </form>
         </div>
     )
