08c16666c29e1db335c774b3fbb6ab6daaaa1bab
AkiraYago <the.programmer.yago@gmail.com>
2023-04-10 09:36:25 -0400

    Index fixed

--- src/components/index.js
+++ src/components/index.js
@@ -10,7 +10,7 @@ import { ProjectCodeEnter } from "./CreateProjects";
 export {
     Banner,
     BannerNoUser,
-    Register
+    Register,
     CreateProject,
     ProjectCodeEnter,
     ProjectCode
