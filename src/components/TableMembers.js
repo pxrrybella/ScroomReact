2e823083f0c6f17ae5ba7c7e8f59e990fdc3735f
DiegoPerez0201 <diegomartinperezmorales@gmail.com>
2023-04-12 11:38:28 -0400

    modal y delete de miembros de un proyecto

--- src/components/TableMembers.js
+++ src/components/TableMembers.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import { userwebGetAll } from "../services/UserwebService";
+import { userProjectDelete } from "../services/UserwebProjectService";
 
 const TableMembers = ({ userProject }) => {
     const [userList, setUserList] = useState([]);
@@ -8,6 +9,11 @@ const TableMembers = ({ userProject }) => {
     const getAll = async () => {
         await userwebGetAll();
         setUserList(await userwebGetAll());
+    }
+
+    const deleteUserProject = async (id) => {
+        await userProjectDelete(id);
+        getAll();
     }
 
     useEffect(() => {
@@ -37,7 +43,26 @@ const TableMembers = ({ userProject }) => {
                             <td>{userList.map(user => user.userweb_id == userProject ? (<p>{user.last_name}</p>) : (<></>))}</td>
                             <td>{userList.map(user => user.userweb_id == userProject ? (<p>{user.email}</p>) : (<></>))}</td>
                             <td>
-                                <button type="button" class="btn btn-danger ms-2">Eliminar</button>
+                                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Eliminar</button>
+                                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                    <div class="modal-dialog">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h1 class="modal-title fs-5" id="exampleModalLabel">¿Estás seguro de eliminar a un miembro del proyecto?</h1>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <p>Si eliminas al miembro del proyecto, tendrá que volver a unirse mediante el código autogenerado</p>
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                                                <a href="/ProjectSetting">
+                                                    <button type="button" class="btn btn-primary" onClick={() => deleteUserProject(userProject)}>Sí, estoy seguro</button>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </td>
                         </tr>
                     ))}
