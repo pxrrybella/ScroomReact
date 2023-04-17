36d568e6536d7aa866b89816a44212c492e16141
DiegoPerez0201 <diegomartinperezmorales@gmail.com>
2023-04-17 12:28:41 -0400

    solucion error ejustes de proyecto

--- src/components/ProjectSetting.js
+++ src/components/ProjectSetting.js
@@ -21,23 +21,7 @@ const ProjectSetting = () => {
                 </div>
             </Link>
             <div className="eliminar text-center m-3">
-                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Eliminar proyecto</button>
-                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                    <div class="modal-dialog">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h1 class="modal-title fs-5" id="exampleModalLabel">¿Deseas eliminar este proyecto?</h1>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                            </div>
-                            <div class="modal-body">
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-                                <button type="button" class="btn btn-primary">Sí, estoy seguro</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <button type="button" class="btn btn-primary">Eliminar proyecto</button>
             </div>
         </form>
     )
