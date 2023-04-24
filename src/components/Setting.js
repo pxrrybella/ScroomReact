aaf22314ac4affb8227fc8842bb1becc2f136d96
ArtemisLuna123 <mplazcano08@gmail.com>
2023-04-24 14:22:27 -0400

    settings

--- src/components/Setting.js
+++ src/components/Setting.js
@@ -11,6 +11,7 @@ const userwebInit = {
     "user_password":"",
     "company_position":"",
     "company_id":1
+
        
 }
   
@@ -19,10 +20,10 @@ const userwebInit = {
 
 const Setting = ({editUserweb,userwebEdited,setUserwebEdited}) => {
     const [userweb, setUserweb] = useState(userwebInit);
-    const {user_name,first_name,last_name,user_password} = userweb;
+    const {user_name,first_name,last_name,user_password,email} = userweb;
 
 const handleInputChange = (u) => {
-    console.log(u);
+    console.log(u + "hangleInputChange ejecutandose");
     const handleFormValue = {
         ...userweb,
         // "email":userweb.email,
@@ -32,7 +33,17 @@ const handleInputChange = (u) => {
         [u.target.name]: u.target.value,
     }
     setUserweb(handleFormValue);
-    console.log(userweb);
+    console.log(userweb);  
+    console.log(u + "hangleInputChange ejecutandose");    
+}
+const current_password = handleInputChange ;
+const handleSave = () => {
+    if (current_password === userweb.user_password) {
+   console.log("Las contraseñas coinciden");
+ //     editUserweb(userweb);
+    } else {
+      alert("Contraseña actual incorrecta");
+    }
 }
     useEffect(()=>{
         if(userwebEdited!==null){
@@ -42,10 +53,11 @@ const handleInputChange = (u) => {
             "user_name":"",
             "first_name":"",
             "last_name":"",
-            "email":"yagura@email.com",
+            "email":"mary.smith@email.com",
             "user_password":"",
             "company_position":"Q.A.",
-            "company_id":2
+            "company_id":2,
+          
         })
 
         }
@@ -58,7 +70,7 @@ const handleInputChange = (u) => {
                 <label for="exampleFormControlInput1" class="form-label"><b>Nombre</b></label>
                 <input type="text" 
                 class="form-control" 
-                placeholder="Nombre"
+                placeholder={first_name}
                 id="first_name"
                 name="first_name"
                 value={first_name}
@@ -68,7 +80,7 @@ const handleInputChange = (u) => {
                 <label for="exampleFormControlInput1" class="form-label"><b>Apellido</b></label>
                 <input type="text"
                  class="form-control" 
-                 placeholder="Apellido"
+                 placeholder={last_name}
                  id="last_name"
                  name="last_name"
                  value={last_name}
@@ -79,7 +91,7 @@ const handleInputChange = (u) => {
                 <label for="exampleFormControlInput1" class="form-label"><b>Nombre de usuario</b></label>
                 <input type="text" 
                 class="form-control"
-                placeholder="Nombre de usuario"
+                placeholder={user_name}
                 id="user_name"
                 name="user_name"
                 value={user_name}
@@ -87,20 +99,44 @@ const handleInputChange = (u) => {
                
             </div>
             <div class="mb-3">
-                <label for="exampleFormControlInput1" class="form-label"><b>Nueva contraseña</b></label>
-                <input type="password" 
+                <label for="exampleFormControlInput1" class="form-label"><b>Email</b></label>
+                <input type="text" 
                 class="form-control"
-                placeholder="contraseña"
-                id="user_password"
-                name="user_password"
-                value={user_password}
+                placeholder={email}
+                id="email"
+                name="email"
+                value={email}
                 onChange={handleInputChange}/>
-            
+               
             </div>
             <div class="mb-3">
                 <label for="disabledTextInput" class="form-label"><b>Contraseña actual</b></label>
                 <p>Verificar cambios</p>
                 <input type="password" id="disabledTextInput" class="form-control" />
+            </div>
+
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label"><b>Nueva contraseña</b></label>
+                <input type="password" 
+                class="form-control"
+                placeholder={user_password}
+                id="user_password"
+                name="user_password"
+                value={user_password}
+                onChange={handleInputChange}/>
+
+            </div>
+           
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label"><b>Confirmar contraseña</b></label>
+                <input type="password" 
+                class="form-control"
+            //    placeholder={user_password}
+                id="confirm_password"
+                name="confirm_password"
+            //    value={user_password}
+                onChange={handleSave} />
+                
             </div>
             <div class="text-center m-3">
             <button class="btn btn-primary" onClick = {()=>editUserweb(userweb)}type="button">Guardar</button>
